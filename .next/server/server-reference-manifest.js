f5a2f796b17861d8ed32acd048722ebb27d1c72d
YoungHoon <kai87yh@gmail.com>
2024-06-24 22:00:22 +0900

    commit webpack build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fN1BsXprKkeKtoFagIIv8M3jCppY0KAzVsFHnGvavRM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vf/lpTGEsFAlQL/evoMbBegvO+ES65zqVEtOkCCsyzU=\"\n}"
