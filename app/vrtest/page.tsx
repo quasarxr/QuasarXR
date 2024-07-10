9f071f5d6c6f8f82861ae52c793259fd24340884
YoungHoon <kai87yh@gmail.com>
2024-07-10 16:32:38 +0900

    add vrtest code

--- app/vrtest/page.tsx
+++ app/vrtest/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 import { VRButton } from 'three/examples/jsm/webxr/VRButton'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 
 export default function page() {
@@ -23,17 +24,30 @@ export default function page() {
         const cube = new THREE.Mesh(geometry, material);
         const light = new THREE.DirectionalLight(0xffffff, 10);
         const ambient = new THREE.AmbientLight(0xffffff);
+        const sphereGeometry = new THREE.SphereGeometry();
+        const material1 = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+        const sphere = new THREE.Mesh( sphereGeometry, material1 );
         scene.add(cube);
         scene.add(light);
         scene.add(ambient);
+        scene.add(sphere);
+        scene.background = new THREE.Color( 0x0c0c0c );
+
+        const loader = new GLTFLoader();
+        loader.load( './mario_animacion.glb', res => {
+            scene.add( res.scene );
+        } )
         
-        camera.position.set(0, 0, 5);
+        camera.position.set(0, 1, 5);
         
         function animate() {
+            console.log( camera );
             renderer.setAnimationLoop(function () {
                 cube.rotation.x += 0.01;
                 cube.rotation.y += 0.01;
                 renderer.render(scene, camera);
+                const time = Date.now() / 1000 ;
+                sphere.position.set( 4 * Math.cos(time), 0, 4 * Math.sin(time) );
             });
         }
         
