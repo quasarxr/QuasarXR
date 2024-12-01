75bfd33aeac67f90a8cdc7d3f9a2527903657d63
YoungHoon <kai87yh@gmail.com>
2024-12-01 14:28:48 +0900

    rename plugins/graph/scene.ts, plugins/graph/tween.ts -> sceneGraph.ts, tweenGraph.ts

--- PalletEngine/gui/plugins/graph/tweenGraph.ts
+++ PalletEngine/gui/plugins/graph/tweenGraph.ts
@@ -12,6 +12,10 @@ import {
     TpPluginBundle
 } from '@tweakpane/core';
 
+import EventEmitter from '../../event';
+
+enum CB_KEY { ADD = "add", REMOVE = "remove", PREVIEW = "preview", MODELUPDATE = 'modelUpdate' };
+
 export class TweenGraphApi extends BladeApi<TweenGraphBladeController> {
 
     public clear() {
@@ -21,8 +25,24 @@ export class TweenGraphApi extends BladeApi<TweenGraphBladeController> {
         this.controller.viewUpdate( data );
     }
 
+    private setCallback( key, callback ) {
+        return this.controller.callbackMap.get( key ).func = callback;
+    }
+
     public onUpdateModel( callback ) {
-        this.controller.modelUpdateCallback.func = callback;
+        this.setCallback( CB_KEY.MODELUPDATE, callback );
+    }
+
+    public onTweenAdd( callback ) {
+        this.setCallback( CB_KEY.ADD, callback );
+    }
+
+    public onTweenRemove( callback ) {
+        this.setCallback( CB_KEY.REMOVE, callback );
+    }
+
+    public onTweenPreview( callback ) {
+        this.setCallback( CB_KEY.PREVIEW, callback );
     }
 }
 
@@ -36,6 +56,9 @@ type TweenCallback = { func : ( ...args : any [] ) => void }
 
 export class TweenGraphBladeController extends BladeController<TweenGraphView> {
 
+    callbackMap : Map<string, TweenCallback>;
+    presets : string[];
+    modelAddCallback : TweenCallback;
     modelUpdateCallback : TweenCallback;
 
     constructor( config : ControllerConfig ) {
@@ -47,7 +70,11 @@ export class TweenGraphBladeController extends BladeController<TweenGraphView> {
             viewProps : config.viewProps,
         }
         super( param );
-        this.modelUpdateCallback = { func : undefined };
+
+        this.callbackMap = new Map<string, TweenCallback>();
+        Object.values( CB_KEY ).forEach( cbName => {
+            this.callbackMap.set( cbName, { func: undefined } );
+        } )
         this.assignViewEvents();
     }
 
@@ -60,7 +87,7 @@ export class TweenGraphBladeController extends BladeController<TweenGraphView> {
     }
 
     private assignViewEvents() {
-        this.view.assignEvents( this.modelUpdateCallback );
+        this.view.assignEvents( this.callbackMap );
     }
 }
 
@@ -79,8 +106,16 @@ export class TweenGraphView implements View {
     private dragStartIndex : Number;
     private dropInsertIndex : Number;
 
+    private selectedElement : HTMLElement;
+
     public readonly element : HTMLElement;
     public readonly graph : HTMLElement;
+
+    public readonly controlArea : HTMLElement;
+    public readonly controlAdd : HTMLElement;
+    public readonly controlRemove : HTMLElement;
+    public readonly controlPreview : HTMLElement;
+
     constructor( doc : Document, config : ViewConfig ) {
         this.document = doc;
         this.element = doc.createElement( 'div' );
@@ -90,6 +125,37 @@ export class TweenGraphView implements View {
         this.graph.classList.add(className('g'));
         
         this.element.appendChild( this.graph );
+
+        this.controlArea = doc.createElement( 'div' );
+        this.controlArea.classList.add( className('control') );
+        this.element.appendChild( this.controlArea );
+
+        this.controlAdd = doc.createElement( 'div' );
+        this.controlAdd.classList.add( className( 'btn' ) );
+        this.controlAdd.textContent = 'Add';
+        this.controlAdd.addEventListener( 'click', event => {
+            EventEmitter.emit('tween-add', undefined);
+        } );
+        this.controlArea.appendChild( this.controlAdd );
+        
+        this.controlRemove = doc.createElement( 'btn' );
+        this.controlRemove.classList.add( className( 'btn' ) );
+        this.controlRemove.textContent = 'Remove';
+        this.controlRemove.addEventListener( 'click', event => {
+            EventEmitter.emit( 'tween-remove', undefined );
+        } );
+        this.controlArea.appendChild( this.controlRemove );
+
+        this.controlPreview = doc.createElement( 'btn' );
+        this.controlPreview.classList.add( className( 'btn' ) );
+        this.controlPreview.textContent = 'Preview';
+        this.controlPreview.addEventListener( 'click', event => {
+            EventEmitter.emit( 'tween-preview', undefined );
+        } );
+        this.controlArea.appendChild( this.controlPreview );
+
+        
+
         this.draggingElement = null;
         this.nextSibiling = null;
         
@@ -127,6 +193,13 @@ export class TweenGraphView implements View {
             console.log( 'drag start index : ', this.dragStartIndex );
         } );
 
+        element.addEventListener( 'mouseup', e => {
+            e.stopPropagation();
+            this.selectedElement?.classList.remove( 'selected' );
+            this.selectedElement = element;
+            element.classList.toggle( 'selected' );
+        } );
+
         element.addEventListener( 'dragstart', event => {
             event.dataTransfer.effectAllowed = 'move';
         } );
@@ -138,12 +211,16 @@ export class TweenGraphView implements View {
         return element;
     }
 
-    assignEvents( modelUpdate : TweenCallback ) {
+    assignEvents( callbacks : Map<string, TweenCallback> ) {
         this.graph.addEventListener( 'click', ev => {
             ev.stopPropagation();
             if ( this.draggingElement ) {
                 this.draggingElement.classList.remove( 'dragging' );                
                 this.draggingElement = null;
+            }
+
+            if ( ev.target === this.graph ) {
+                this.deSelectItem();
             }
         } );
 
@@ -188,11 +265,8 @@ export class TweenGraphView implements View {
                 this.draggingElement = null;
                 this.nextSibiling = null;
 
-                if ( modelUpdate.func ) {
-                    modelUpdate.func( this.dragStartIndex, this.dropInsertIndex );
-                } else {
-                    console.log( 'model null' );
-                }
+                callbacks.get(CB_KEY.MODELUPDATE)?.func( this.dragStartIndex, this.dropInsertIndex );
+
             }
 
             this.dragStartIndex = -1;
@@ -203,6 +277,30 @@ export class TweenGraphView implements View {
     clearGraph() {
         while( this.graph.firstChild ) {
             this.graph.removeChild( this.graph.firstChild );
+        }
+    }
+
+    toggleSelect( element : HTMLElement ) {
+        element?.classList.toggle( 'selected' );
+    }
+
+    selectItem( element : HTMLElement ) {
+        if ( element == undefined || element == null ) console.error( 'element has to passed with parameter' );
+        if ( this.selectedElement ) {
+            this.deSelectItem();
+            this.selectedElement = element;
+        }
+        if ( ! element.classList.contains( 'selected' ) ) {
+            element.classList.add( 'selected' );
+        }
+    }
+
+    deSelectItem( element : HTMLElement = undefined ) {
+        if ( element ) {
+            element.classList.remove( 'selected' );
+        } else {
+            this.selectedElement?.classList.remove( 'selected' );
+            this.selectedElement = null;
         }
     }
 }
@@ -244,13 +342,14 @@ export const TweenGraphBundle : TpPluginBundle = {
     ],
     css : `.tp-tween-graphv_g {
         height: 250px;
-        border-radius: 5px;
+        border-radius: 5px 5px 0px 0px;
         border: 1px solid black;
-        background-color: rgb( 107, 107, 107 );
+        border-width: 1px 1px 0px 1px;
+        background-color: rgb( 105, 105, 105 );
         margin: 0px 3px;
         overflow-y: auto;
         scrollbar-width: thin;
-        scrollbar-color: #ccc rgba(95, 95, 95, 0.4);
+        scrollbar-color: #ccc rgba(105, 105, 105, 0.4);
     }
     .tp-tween-graphv_item {
         margin: 3px;
@@ -267,18 +366,47 @@ export const TweenGraphBundle : TpPluginBundle = {
 
     .tp-tween-graphv_item:hover {
         background: blue;
-    }
-
-    .tp-tween-graphv_item:active {
-        background: white;
-    }
-    
+    }    
     .tp-tween-graphv_item.dragging {
         background: yellow;
     }
     .tp-tween-graphv_item.nextItem {
         background: red;
         margin-top : 20px;
+    }        
+    .tp-tween-graphv_item.selected {
+        background: white;
+    }
+
+    .tp-tween-graphv_control {
+        display: flex;
+        flex-direction: row;
+        justify-content: end;
+        margin: 0px 3px;
+        background-color: rgb( 105, 105, 105 );
+        border-radius: 0px 0px 5px 5px;
+        border: 1px solid black;
+        border-width: 0px 1px 1px 1px;
+
+    }
+    .tp-tween-graphv_btn {
+        border-radius: 2px;
+        margin: 4px 4px;
+        background-color: rgb(55,55,55);
+        padding: 5px;
+        width: auto;
+        color: rgb(125,125,125);
+        font-weight: bold;
+        user-select: none;
+        cursor: pointer;
+        transition: background-color 0.1s;
+    }
+    .tp-tween-graphv_btn:hover {
+        background-color: rgb(75,75,75);
+    }
+    .tp-tween-graphv_btn:active {
+        background-color: rgb(100,100,100);
+        box-shadow: 1px 1px 1px rgb( 125, 125, 125 );
     }
     `
 };
