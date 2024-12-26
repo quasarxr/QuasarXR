3a99b76f3a3a5a3ba97918d60aee55e2d9444667
YoungHoon <kai87yh@gmail.com>
2024-12-27 03:44:24 +0900

    add implementation file download

--- PalletEngine/utils/file.ts
+++ PalletEngine/utils/file.ts
@@ -8,6 +8,21 @@ export default class FileUtil {
         f.click();
         return f;
     }
+
+    static DownloadFile( fileName : string, data : any ) {
+        try {
+            const json = JSON.stringify( data );
+            const blob = new Blob( [ json ], { type: 'application/json' } );
+            const url = URL.createObjectURL( blob );
+            const a = document.createElement( 'a' );
+            a.href = url;
+            a.download = fileName;
+            a.click();
+            URL.revokeObjectURL( url );
+        } catch( error ) {
+            console.error( error );
+        }
+    }
     
     static FileExtension( path ) {
 
