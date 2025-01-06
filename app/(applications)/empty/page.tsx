bae150f8b67e3903b99032484391c80afc7cfd3a
YoungHoon <kai87yh@gmail.com>
2025-01-07 04:17:58 +0900

    move setup authentication to PalletComponent - 1

--- app/(applications)/empty/page.tsx
+++ app/(applications)/empty/page.tsx
@@ -1,90 +1,20 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
-import * as THREE from 'three'
-import '@/styles/global.css'
-import { useSession } from 'next-auth/react';
-import styles from './styles.module.css';
-import { login, logout } from '@/app/actions/auth';
-import { LoginFormState } from '@/app/lib/definitions';
-import LoginForm from '@/app/components/login/login';
-
-let PalletPromise = null;
+import '@/styles/global.css';
+import PalletComponent from '@/app/components/engine/palletComponent';
 
 export default function Page() {
-  //strict mode
-  function forward() {
-    window.history.forward();
-  }
-  function back() {
-    window.history.back();
-  }
-    
-  const { data : session, status } = useSession();
-  const engineRef = useRef( null );
-  const sessionRef = useRef( null );
-  const router = useRouter();
-  const pathName = usePathname();
-
-  const [showLogin, setShowLogin ] = useState(false);
-  const handleLoginClick = () => setShowLogin(true);
-
-  const updateSession = ( session, status, callback ) => {
-    if ( engineRef.current !== null ) {
-      callback();
-      engineRef.current.updateSession( session );
-      console.log( session, status );
-
-
-    }
-  }
-
-  useEffect( () => {
-    if ( PalletPromise === null ) {
-      PalletPromise = import( '../../../PalletEngine/module' );
-      PalletPromise.then( pallet => {
-        pallet._engineFactory( { mode : 'editor' }, ( engine ) => {
-          console.log( engine, 'engine initialized' );
-          engineRef.current = engine;
-
-          pallet._createAuthController( null, () => {
-            console.log( 'login registered' );
-            handleLoginClick();
-          }, () => {
-            console.log( 'logout registered' );
-            logout().then( () => {
-              console.log( 'logout' );
-              //engineRef.current.updateSession( null );
-            } );
-          } );
-
-          if ( sessionRef.current !== null ) {
-            updateSession( sessionRef.current, undefined, () => console.log('engine first') );
-          }
-        } );
-      } );
-    }
-  }, [] );
-
-  useEffect( () => {
-    sessionRef.current = session;
-    updateSession( session, status, () => console.log('session first') );
-  }, [ session, status ] );
 
   const styleText = {
     width: "100%",
     height: "100%",
     background: "#3c3c3c"
-  } ;
-
-
+  };
 
   return (
   <div style={styleText}>    
     <div>
-      <LoginForm isLogin={showLogin} setIsLogin={setShowLogin} redirect={pathName}/>
-      <canvas style={styleText} className="view"></canvas>
+      <PalletComponent url={""} mode={"editor"} size={{ width : "100vw", height: "100vh" }} scroll={false}></PalletComponent>
     </div>
   </div> );
 }
--- app/components/engine/palletComponent.tsx
+++ app/components/engine/palletComponent.tsx
@@ -1,31 +1,67 @@
 'use client'
 
 import LoadingComponent from '../loading/loadingComponent';
-import { CSSProperties, useEffect, forwardRef, useRef, useImperativeHandle } from 'react';
+import { CSSProperties, useEffect, forwardRef, useRef, useImperativeHandle, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import { logout } from '@/app/actions/auth';
+import LoginForm from '@/app/components/login/login';
+import style from './style.module.css';
 
 export interface PalletComponentRef {
     /** */
 }
 
+//strict mode
+// function forward() {
+//     window.history.forward();
+// }
+
+// function back() {
+//     window.history.back();
+// }
+
 interface Props {
     mode? : string;
     url? : string;
     onload? : (() => void) | [() => void];
+    size? : { width: string, height: string }
+    scroll? : boolean;
 }
 
-const PalletComponent = forwardRef<PalletComponentRef, Props>( ( { url, mode, onload } : Props, ref ) => {
+const _scrollKeyword = 'no-scroll';
+
+const PalletComponent = forwardRef<PalletComponentRef, Props>( ( { url, mode, onload, size, scroll = true } : Props, ref ) => {
     let PalletPromise = null;
     let engineInstance = useRef( null );
     useImperativeHandle( ref, () => ({
     }));
+    
+    const currentPath = usePathname();
     const canvasRef = useRef(null);
     const loadingRef = useRef(null);
+    const sessionRef = useRef(null);
+    const { data : session } = useSession();
 
-    const style : CSSProperties = { display: 'inline-block', position: 'relative', width: 'inherit', height: 'inherit' };
+    const style : CSSProperties = { display: 'inline-block', position: 'relative', width: size ? size.width : 'inherit', height: size ? size.height : 'inherit', };
+
+    // declare session
+    // declare login form
+    const [ showLogin, setShowLogin ] = useState(false);
+    const loginCallback = () => setShowLogin(true);
+    const logoutCallback = () => logout().then( () => {} );
+
+    const updateEngineSession = ( session, callback ) => {
+        if ( engineInstance.current !== null ) {
+            callback();
+            console.log( session );
+            engineInstance.current.updateSession( session );
+        }
+    }
 
     useEffect( () => {
+
         if ( PalletPromise === null ) {
-            console.log( canvasRef.current.clientWidth, canvasRef.current.clientHeight );
             PalletPromise = import ( '@/PalletEngine/module' );
             PalletPromise.then( pallet => {
                 pallet._engineFactory( { mode : mode }, ( engine ) => {
@@ -39,15 +75,33 @@ const PalletComponent = forwardRef<PalletComponentRef, Props>( ( { url, mode, on
                         loadingRef.current.hide();
                     }
                     //engine.resizeRenderer( { canvas : canvasRef.current } );
+
+                    if ( sessionRef.current !== null ) {
+                        updateEngineSession( sessionRef.current, () => {} );
+                    }
+                    
+                    pallet._createAuthController( null, loginCallback, logoutCallback );
                 } );
             } );
         }
+
+        if ( !scroll ) {
+            document.body.classList.add( _scrollKeyword );
+            return () => document.body.classList.remove( _scrollKeyword );
+        }
+
     } );
 
+    useEffect( () => {
+        sessionRef.current = session;
+        updateEngineSession( session, () => console.log( 'session updated' ) );
+    }, [ session ] );
+
     return (
-        <div id='canvas-container' style={style}>            
+        <div id='canvas-container' style={style}>
+            <LoginForm isLogin={showLogin} setIsLogin={setShowLogin} redirect={currentPath}></LoginForm>
             <LoadingComponent ref={loadingRef} style={{zIndex: 2}}/>
-            <canvas className="view" ref={canvasRef} style={style}></canvas>
+            <canvas className="view" ref={canvasRef} style={{width: "100%", height: "100%" } }></canvas>
         </div>
     )
 });
